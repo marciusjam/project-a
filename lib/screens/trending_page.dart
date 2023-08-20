ae79377717b1286101c592e3084020d0ba3037fc
marciusjam <johnmarcius21@gmail.com>
2023-08-21 01:44:01 +0800

    asd

---
+++ lib/screens/trending_page.dart
@@ -0,0 +1,16 @@
+import 'package:agilay/widgets/home_bar.dart';
+import 'package:agilay/widgets/post_card.dart';
+import 'package:flutter/material.dart';
+import 'package:amplify_flutter/amplify.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TrendingPage extends StatelessWidget {
+  const TrendingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
